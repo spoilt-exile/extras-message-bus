91ccf3dbe7079da025fd49fa34dc34a31662a5ec
Stanislav Nepochatov <spoilt@blackstarfilm.com>
2020-04-18 20:03:08 +0300

    Add deserialization adapter for ZonedDateTime in GSON, fixes issues with storage;

--- src/main/java/tk/freaxsoftware/extras/bus/bridge/http/util/ZonedDateTimeDeserializer.java
+++ src/main/java/tk/freaxsoftware/extras/bus/bridge/http/util/ZonedDateTimeDeserializer.java
@@ -18,26 +18,23 @@
  */
 package tk.freaxsoftware.extras.bus.bridge.http.util;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonSerializer;
+import java.lang.reflect.Type;
 import java.time.ZonedDateTime;
 
 /**
- * GSON utils class.
+ * GSON deserializer for date time.
  * @author Stanislav Nepochatov
  */
-public class GsonUtils {
-    
-    private static Gson gson;
-    
-    public static Gson getGson() {
-        if (gson == null) {
-            gson = new GsonBuilder()
-                    .registerTypeAdapter(ZonedDateTime.class, new ZonedDateTimeSerializer())
-                    .registerTypeAdapter(ZonedDateTime.class, new ZonedDateTimeDeserializer())
-                    .create();
-        }
-        return gson;
+public class ZonedDateTimeDeserializer implements JsonDeserializer<ZonedDateTime> {
+
+    @Override
+    public ZonedDateTime deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+        return ZonedDateTime.parse(json.getAsString());
     }
     
 }
